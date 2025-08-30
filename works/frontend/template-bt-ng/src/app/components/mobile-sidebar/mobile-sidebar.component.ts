e2be3ff7cc2547b5035490c72c6221d16af3def1
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2025-08-30 23:05:25 +0200

    🎨 Installation du template Stellar Angular - étape 2g - modification du titre de l'application. - ajout de la fermeture automatique de la mobile-sidebar. - mise à jour de la version de production de l'application (TemplateBtNg v0.1.1). - test de l'application hébergée sur desktop et mobile (huawai P40).

--- works/frontend/template-bt-ng/src/app/components/mobile-sidebar/mobile-sidebar.component.ts
+++ works/frontend/template-bt-ng/src/app/components/mobile-sidebar/mobile-sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-mobile-sidebar',
@@ -6,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './mobile-sidebar.component.html',
   styleUrl: './mobile-sidebar.component.scss'
 })
-export class MobileSidebarComponent {
+export class MobileSidebarComponent implements AfterViewInit {
+  @ViewChild('closeBtn') closeBtnRef!: ElementRef<HTMLButtonElement>;
 
+  constructor(private router: Router) { }
+
+  ngAfterViewInit(): void {
+    // fermeture de la sidebar à chaque fin de navigation
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.closeBtnRef.nativeElement.click();
+      });
+  }
 }
